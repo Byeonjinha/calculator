ecfbf3a0e775b7ec3a7c341ebbb4262c28351d2f
Byeonjinha <87685946+Byeonjinha@users.noreply.github.com>
2022-05-20 14:27:52 +0900

    calculator

---
+++ calculator/calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by Byeon jinha on 2022/05/20.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
